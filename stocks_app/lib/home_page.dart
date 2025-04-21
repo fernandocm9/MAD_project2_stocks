0be132bdad8747ac4f544fe0b46485ea29ec0aeb
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-04-21 15:53:39 -0400

    registering and signing in works. Added a sign out button to the scaffold of the homepage to log out properly.

--- stocks_app/lib/home_page.dart
+++ stocks_app/lib/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'auth_gate.dart';
 
-
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
@@ -12,27 +12,41 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signOut() async {
+    await _auth.signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const AuthGate()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final userEmail = _auth.currentUser?.email ?? 'No user logged in';
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stocks'),
+        title: const Text('StockTracker'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: _signOut,
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(
+              'Welcome, $userEmail!',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
             const Text(
               'Welcome to the Stocks App!',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AuthGate()),
-                );
-              },
-              child: const Text('Go to Auth Gate'),
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
