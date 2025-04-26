01cd60ae346b6fa94e1ef19d8cf694393e962bef
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-04-25 21:00:12 -0400

    added the foundation for watchlist tab where it retrieves the watchlist and displays them on the screen for now

--- stocks_app/lib/watchlist.dart
+++ stocks_app/lib/watchlist.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'auth_gate.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class WatchlistPage extends StatefulWidget {
   const WatchlistPage({super.key});
@@ -12,20 +10,45 @@ class WatchlistPage extends StatefulWidget {
 }
 
 class _WatchlistPageState extends State<WatchlistPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'watchlist goes here',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
+    final User? user = _auth.currentUser;
+
+    if (user == null) {
+      return const Center(child: Text('No user logged in.'));
+    }
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('watchlists').where('user_id', isEqualTo: user.uid).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return const Center(child: Text('Error loading watchlist.'));
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No stocks in your watchlist.'));
+        }
+
+        final watchlistStocks = snapshot.data!.docs.map((doc) {
+          return doc['symbol'] as String;
+        }).toList();
+
+        return ListView.builder(
+          itemCount: watchlistStocks.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(watchlistStocks[index]),
+            );
+          },
+        );
+      },
     );
   }
 }
